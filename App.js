f094e00606932dbb3a39d6da812b9e617a9d0b49
RustamNur <136016020+RustamNur@users.noreply.github.com>
2023-09-07 14:22:00 +0300

    Conditional_Rendering

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import "./App.css";
+import Login from "./components/login/login.jsx";
+import Signup from "./components/signup/signup.jsx";
+
+function App() {
+  const [change,  setChange] = useState(true)
+
+  const reDirect = (value) => {
+    console.log(value)
+    setChange(value)
+  }
+
+  return (
+    <div>
+       {change ? <Login onDirect={reDirect}/> : <Signup onDirect={reDirect}/>}
+    </div>
+  );
+}
+
+export default App;
